b39e36d623a8b20df4c725664d1bbdea82ba06ad
Alex Kolarski <aleks.rk@gmail.com>
2018-09-09 15:49:59 +0300

    Typescript hooks

--- src/codeGen/processingHooksTemplate.js
+++ src/codeGen/processingHooksTemplate.js
@@ -1,34 +1,34 @@
 export default {
   Root: {
-    queryPreprocess(root, args, context, ast) {
+    queryPreprocess(root: any, args: any, context: any, ast: any) {
       //Called before query filters are processed
     },
-    queryMiddleware(queryPacket, root, args, context, ast) {
+    queryMiddleware(queryPacket: any, root: any, args: any, context: any, ast): any {
       //Called after query filters are processed, which are passed in queryPacket
     },
-    queryPreAggregate(aggregateItems, root, args, context, ast) {
+    queryPreAggregate(aggregateItems: any, root: any, args: any, context: any, ast: any) {
       //Called right before a Mongo aggregation is performed
     },
-    beforeInsert(objToBeInserted, root, args, context, ast) {
+    beforeInsert(objToBeInserted: any, root: any, args: any, context: any, ast: any) {
       //Called before an insertion occurs. Return false to cancel it
     },
-    afterInsert(newObj, root, args, context, ast) {
+    afterInsert(newObj: any, root: any, args: any, context: any, ast: any) {
       //Called after an object is inserted
     },
-    beforeUpdate(match, updates, root, args, context, ast) {
+    beforeUpdate(match: any, updates: any, root: any, args: any, context: any, ast: any) {
       //Called before an update occurs. Return false to cancel it
     },
-    afterUpdate(match, updates, root, args, context, ast) {
+    afterUpdate(match: any, updates: any, root: any, args: any, context: any, ast: any) {
       //Called after an update occurs. The filter match, and updates objects will be
       //passed into the first two parameters, respectively
     },
-    beforeDelete(match, root, args, context, ast) {
+    beforeDelete(match: any, root: any, args: any, context: any, ast: any) {
       //Called before a deletion. Return false to cancel it.
     },
-    afterDelete(match, root, args, context, ast) {
+    afterDelete(match: any, root: any, args: any, context: any, ast: any) {
       //Called after a deltion. The filter match will be passed into the first parameter.
     },
-    adjustResults(results) {
+    adjustResults(results: any) {
       //Called anytime objects are returned from a graphQL endpoint. Use this hook to make adjustments to them.
     }
   }
